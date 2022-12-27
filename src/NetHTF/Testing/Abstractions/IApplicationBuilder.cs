bf42ac6ac00b1df7c3052058ea99a8d27ee6742b
shinus9008 <shinus9008@gmail.com>
2022-12-27 15:53:44 +0500

    Add empty project

---
+++ src/NetHTF/Testing/Abstractions/IApplicationBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetHTF.Testing.Abstractions
+{
+    public interface IApplicationBuilder
+    {
+    }
+}
---
+++ src/NetHTF/Testing/Builder/ApplicationBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetHTF.Testing.Builder
+{
+    public class ApplicationBuilder
+    {
+    }
+}
