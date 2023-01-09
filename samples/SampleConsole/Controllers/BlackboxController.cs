c6936d6e6c533b69e482a723b138f852f630c60f
shinus9008 <shinus9008@gmail.com>
2023-01-09 16:57:42 +0500

    add sketch

---
+++ samples/SampleConsole/Controllers/BlackboxController.cs
@@ -0,0 +1,40 @@
+﻿using static SampleConsole.Controllers.BlackboxController;
+
+namespace SampleConsole.Controllers
+{
+    [BlackboxRow(In1 = true,  In2 = true,  Out = true)]
+    [BlackboxRow(In1 = true,  In2 = false, Out = false)]
+    [BlackboxRow(In1 = false, In2 = true,  Out = false)]
+    [BlackboxRow(In1 = false, In2 = false, Out = false)]
+    public class BlackboxController : Controller<BlackboxRow>
+    {
+
+        public void Do()
+        {
+            //Do relay connection
+        }
+
+        [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+        public sealed class BlackboxRow : Attribute
+        {
+            public bool In1 { get; init; }
+            public bool In2 { get; init; }
+            public bool Out { get; init; }
+        }
+    }
+
+
+
+
+
+
+
+
+    public abstract class Controller
+    {
+    }
+    public abstract class Controller<T>
+    {
+        public T Model { get; private set; }
+    }
+}
---
+++ samples/SampleConsole/Models/BlackboxModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace SampleConsole.Models
+{
+    /// <summary>
+    /// Example black box. AND function.
+    /// </summary>   
+    public class BlackboxModel
+    {
+        private readonly BehaviorSubject<bool> In1;
+        private readonly BehaviorSubject<bool> In2;
+
+        public IObservable<bool> Out { get; }
+
+        public BlackboxModel()
+        {
+            In1 = new BehaviorSubject<bool>(false);
+            In2 = new BehaviorSubject<bool>(false);
+            Out = Observable
+                .CombineLatest(In1, In2, (in1, in2) => in1 & in2 )
+                .Publish()
+                .Retry(1);
+        }
+    }
+}
