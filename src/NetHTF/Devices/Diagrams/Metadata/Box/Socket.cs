7713136278a6f9b93a63c5c9e6d2ed5d582c9dec
shinus9008 <shinus9008@gmail.com>
2023-03-06 00:53:16 +0500

    Edit WithePin method

--- src/NetHTF/Devices/Diagrams/Metadata/Box/Socket.cs
+++ src/NetHTF/Devices/Diagrams/Metadata/Box/Socket.cs
@@ -2,20 +2,16 @@
 {
     public class Socket
     {
-        public Socket(string name)
+        public Socket(string name, IEnumerable<Pin> enumerablePin)
         {
             Name = name;
+            Pins = new List<Pin>(enumerablePin);
         }
 
         public IList<Pin> Pins { get; }
         public string Name { get; }
-        public static Socket WithePin(string soketName, string pinName) =>
-            new Socket(soketName)
-            {
-                Pins =
-                {
-                    new Pin(pinName)
-                }
-            };
+
+        public static Socket WithePin(string soketName, params string[] pinNames) =>
+            new Socket(soketName, pinNames.Select(name => new Pin(name)));           
     }
 }
