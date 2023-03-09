13f0b92953a1ccc2297d45910df6dfcc8e0b1ef6
shinus9008 <shinus9008@gmail.com>
2023-03-09 14:26:21 +0500

    add IFeatureCollection to the HardwareTestContext

--- src/Workspace/Testing/Shinus9008.NetHTF.Workspace.Testing.Abstractions/HardwareTestContext.cs
+++ src/Workspace/Testing/Shinus9008.NetHTF.Workspace.Testing.Abstractions/HardwareTestContext.cs
@@ -1,9 +1,15 @@
-﻿namespace Shinus9008.NetHTF.Workspace.Testing
+﻿using Shinus9008.NetHTF.Workspace.Features;
+
+namespace Shinus9008.NetHTF.Workspace.Testing
 {
     /// <summary>
     /// Encapsulate all information about hardware test
     /// </summary>
     public abstract class HardwareTestContext
     {
+        /// <summary>
+        /// Get <see cref="IFeatureCollection"/>
+        /// </summary>
+        public abstract IFeatureCollection Features { get; }
     }
 }
